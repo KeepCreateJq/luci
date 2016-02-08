1bd45251f478d1644357e03ab0457de827a0951f
Hostle <hostle19@gmail.com>
2016-02-07 23:45:07 -0500

    Update users.lua
    total re-wite of users controller

--- applications/luci-app-multi-user/luasrc/controller/admin/users.lua
+++ applications/luci-app-multi-user/luasrc/controller/admin/users.lua
@@ -1,53 +1,135 @@
-
---[[
-LuCI - Lua Configuration Interface
-
-Copyright 2008 Steven Barth <steven@midlink.org>
-Copyright 2011 Jo-Philipp Wich <xm@subsignal.org>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-	http://www.apache.org/licenses/LICENSE-2.0
-
-$Id$
-]]--
-
---[[
-LuCI - Lua Configuration Interface
-$Id: users.lua 12/12/2014 by Hostle 
-]]--
+-- Copyright 2008 Steven Barth <steven@midlink.org>
+-- Copyright 2011-2015 Jo-Philipp Wich <jow@openwrt.org>
+-- Licensed to the public under the Apache License 2.0.
 
 module("luci.controller.admin.users", package.seeall)
 
-function index()
-local nw = require "luci.dispatcher"
+require ("uci")
+local usw = require "luci.users"
 
+function index()
+	local page
+	local nw = require "luci.dispatcher"
 	local user = nw.get_user()
+	local uci = uci.cursor()
+	local fs = require "nixio.fs"
+
 	if user == "root" then
-	  entry({"admin", "users"}, alias("admin", "users", "users"), _("Edit Users"), 55).index = true
-	  entry({"admin", "users", "users"}, cbi("admin_users/users"), _("User Options"), 60)
+	  page = node("admin", "users")
+	  page.target = firstchild()
+	  page.title  = _("Users")
+	  page.order  = 50
+	  page.index  = true
+		
+		page = entry({"admin", "users", "add_user"}, cbi("admin_users/add_user"), nil)
+		page.leaf = true
+
+		page = entry({"admin", "users", "edit_user"}, cbi("admin_users/edit_user"), nil)
+		page.leaf = true
+
+		page = entry({"admin", "users", "user_delete"}, call("user_delete"), nil)
+		page.leaf = true
+
+		page = entry({"admin", "users", "user_status"}, call("user_status"), nil)
+		page.leaf = true
+
+		page = entry({"admin", "users", "users"}, arcombine(cbi("admin_users/users"), cbi("admin_users/users")), _("Edit Users"), 10)
+		page.leaf   = true
+		page.subindex = true
+
+		if page.inreq then
+			uci:foreach("users", "user",
+				function (section)
+					local ifc = section[".name"]
+					if ifc ~= "loopback" and ifc ~= "new" then
+						entry({"admin", "users", "users", ifc},
+						true, ifc:upper())
+					end
+				end)
+		end
+		
 	end
 	if user ~= "root" then
 	  name = string.sub(user:upper(),0,1) .. user:sub(2,-1)
-	  entry({"admin", "users"}, alias("admin", "users", "passwd"), _(name.."s Options"), 55).index = true
-	  entry({"admin", "users", "passwd"}, cbi("admin_users/passwd"), _("Password"), 62)
+	  page = node("admin", "users")
+	  page.target = firstchild()
+	  page.title  = _(name.."s Options")
+	  page.order  = 50
+	  page.index  = true
+
+	  	page = entry({"admin", "users", "passwd"}, cbi("admin_users/passwd"), _("Password"), 10)
 	end
+
+
 end
 
---## modified passwd function from system.lua, usses user to determine which users password to change ##--
-function action_passwd()
-	local p1 = luci.http.formvalue("pwd1")
-	local p2 = luci.http.formvalue("pwd2")
-	local stat = nil
+local function cdate(user)
+  local udate = nixio.fs.stat("/home/"..user)
+  local first = os.date("%c", udate.ctime)
+  return first
+end
 
-	if p1 or p2 then
-		if p1 == p2 then
-			stat = luci.sys.user.setpasswd(user, p1)
-		else
-			stat = 10
-		end
-	end
-	luci.template.render("admin_users/passwd", {stat=stat})
+
+local function adate(user)
+  local udate = nixio.fs.stat("/home/"..user)
+  last = os.date("%c", udate.atime) or nil
+  return last
+end
+
+local function get_shell(user)
+  local uci = uci.cursor()
+  local shell = uci:get("users", user, "shell")
+ return shell
+end
+
+local function get_group(user)
+  local uci = uci.cursor()
+  local group = uci:get("users", user, "group")
+ return group
+end
+
+function user_status(users)
+  local rv   = { }
+  local user 
+  for user in users:gmatch("[%w%.%-_]+") do
+    local first = cdate(user)
+    local last = adate(user)
+    local shell = get_shell(user)
+    local group = get_group(user)
+    if user then
+      local data = {
+		     id		= user,
+		     shell      = shell,
+		     group      = group,
+		     first	= first,
+		     last 	= last
+	
+		   }
+
+      rv[#rv+1] = data
+    else
+      rv[#rv+1] = {
+		    id   	= user,
+		    shell 	= "unknown",
+		    group 	= "unknown"
+		  }
+    end
+  end
+  if #rv > 0 then
+    luci.http.prepare_content("application/json")
+    luci.http.write_json(rv)
+   return
+  end
+ luci.http.status(404, "No such device")
+end
+
+function user_delete(user)
+  local usw = require "luci.users"
+  local uci = uci.cursor()
+  local rem = uci:delete("users",user)
+	
+  uci:commit("users")
+  usw.del_user(user)
+  luci.http.redirect(luci.dispatcher.build_url("admin/users/users"))
+ return
 end
