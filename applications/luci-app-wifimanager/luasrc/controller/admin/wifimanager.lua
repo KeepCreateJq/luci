3479fd897d68cb59d810121189657d6f0d2c1482
Hostle <hostle19@gmail.com>
2016-02-22 23:40:56 -0500

    Update wifimanager.lua

--- applications/luci-app-wifimanager/luasrc/controller/admin/wifimanager.lua
+++ applications/luci-app-wifimanager/luasrc/controller/admin/wifimanager.lua
@@ -3,13 +3,13 @@ LuCI - Lua Configuration Interface
 $Id: wifimanager.lua 2/17/2016 by Hostle 
 ]]--
 
-module("luci.controller.admin.wifimanagerr", package.seeall)
+module("luci.controller.admin.wifimanager", package.seeall)
 
 
 function index()
 
 	entry({"admin", "wifimanager"}, alias("admin","wifimanager", "wifimanager"), _("Wifi Manger"), 66).index = true
-	entry({"admin","wifimanager", "wifimanager"}, cbi("admin_wifimanager/config"), _("Config"), 60)
+	entry({"admin","wifimanager", "wifimanager"}, cbi("admin_wifimanager/config"), _("Settings"), 60)
 	entry({"admin","wifimanager", "Networks"}, cbi("admin_wifimanager/networks"), _("Networks"), 61)
 end
 
