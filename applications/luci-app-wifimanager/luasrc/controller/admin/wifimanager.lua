1f7d2a317ab6ec252905a64fd86cac9ac6a018df
Hostle <hostle19@gmail.com>
2016-02-22 23:37:01 -0500

    Update and rename applications/luci-app-wifimanager/luasrc/controller/mobile/WifiManager.lua to applications/luci-app-wifimanager/luasrc/controller/admin/wifimanager.lua

---
+++ applications/luci-app-wifimanager/luasrc/controller/admin/wifimanager.lua
@@ -0,0 +1,36 @@
+--[[
+LuCI - Lua Configuration Interface
+$Id: wifimanager.lua 2/17/2016 by Hostle 
+]]--
+
+module("luci.controller.admin.wifimanagerr", package.seeall)
+
+
+function index()
+
+	entry({"admin", "wifimanager"}, alias("admin","wifimanager", "wifimanager"), _("Wifi Manger"), 66).index = true
+	entry({"admin","wifimanager", "wifimanager"}, cbi("admin_wifimanager/config"), _("Config"), 60)
+	entry({"admin","wifimanager", "Networks"}, cbi("admin_wifimanager/networks"), _("Networks"), 61)
+end
+
+
+
+function action_start()
+  wm.start_wifiMgr()
+ return
+end
+
+function action_restart()
+  wm.restart_wifiMgr()
+ return
+end
+
+function action_clear()
+  wm.clear_log()
+ return
+end
+
+function action_stop()
+  wm.stop_wifiMgr()
+ return
+end
