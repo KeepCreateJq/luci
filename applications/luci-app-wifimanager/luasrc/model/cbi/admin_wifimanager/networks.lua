57b1162dd4f3af329aea6c621dc2d2d58171de7a
Hostle <hostle19@gmail.com>
2016-03-15 17:33:07 -0400

    Update networks.lua

--- applications/luci-app-wifimanager/luasrc/model/cbi/admin_wifimanager/networks.lua
+++ applications/luci-app-wifimanager/luasrc/model/cbi/admin_wifimanager/networks.lua
@@ -4,45 +4,11 @@ $Id: wifimanager.lua 2/17/2016
 $ hostle@fire-wrt.com
 ]]--
 
+local sys = require ("luci.sys")
+local version = sys.exec("wifimanager -s")
 local m, s, t, o
 
-m = Map("wifimanager", translate("Wifi Manager"), translate("Here you can configure the Networks to be used by Wifi Manager"))
-
---
--- AP
---
-
-t = m:section(TypedSection, "ap", translate("AP Network"),
-		translate("Enter the default device AP information here"))
-t.anonymous = true
-
-t:tab("apn",  translate("Access Point"))
-
-t.template = "cbi/tblsection"
-
-o = t:taboption("apn", Value, "ap_ssid", translate("SSID"))
-o.default = "Dummy"
-o.rmempty = false
-
-o = t:taboption("apn", ListValue, "ap_encrypt", translate("Encyption Type"))
-o.default = "none"
-o.rmempty = false
-o:value("none", "No Encryption")
-o:value("wep-open", "Wep Open")
-o:value("wep-shared", "No Wep Shared")
-o:value("psk", "WPA-PSK")
-o:value("psk2", "WPA-PSK2")
-o:value("psk-mixed", "WPA-PSK/WPA2-PSK Mixed Mode")
-
-o = t:taboption("apn", Value, "ap_key", translate("Password"))
-o.rmempty = true
-o.password = true
-o:depends("ap_encrypt", "wep-open")
-o:depends("ap_encrypt", "wep-shared")
-o:depends("ap_encrypt", "psk")
-o:depends("ap_encrypt", "psk2")
-o:depends("ap_encrypt", "psk-mixed")
-
+m = Map("wifimanager", translate("Wifi Manager "..version), translate("Here you can configure the Networks to be used by Wifi Manager"))
 
 --
 -- Trusted Networks
@@ -83,5 +49,6 @@ o:depends("encrypt", "wep-shared")
 o:depends("encrypt", "psk")
 o:depends("encrypt", "psk2")
 o:depends("encrypt", "psk-mixed")
+
 
 return m
