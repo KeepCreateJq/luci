6e583e3ee53b885432d86d938cbcd84691e16fc2
Hostle <hostle19@gmail.com>
2016-03-15 17:25:17 -0400

    Update force.lua

--- applications/luci-app-wifimanager/root/usr/lib/lua/wifimanager/force.lua
+++ applications/luci-app-wifimanager/root/usr/lib/lua/wifimanager/force.lua
@@ -7,7 +7,6 @@ local M = {}
 require ("uci")
 local logger = require ("wifimanager.logger")
 local net = require ("wifimanager.net")
-local scan = require ("wifimanager.scan")
 local util = require ("wifimanager.utils")
 
 local get_fnet = function()
@@ -18,42 +17,18 @@ local get_fnet = function()
 end
 M.get_fnet = get_fnet
 
---## SCAN FOR FORCED NETWORK ##--
-local find_fnet = function(fn)
-  if util.not_sane() then return false end
-  if util.has_pending() then 
-    logger.log(2,"{ find_fnet function } A UCI CONFIG HAS PENDING CHANGES ")
-    util.wait() 
-  end
-  logger.log(1,"{find_fnet func} SCANNING FOR FORCED NETWORK: [ "..fn.." ]")
-  local ssta = scan.net_scan("phy0")
-
-  if fn then
-    for i,v in ipairs(ssta) do
-      if v[1] == fn then
-        if net.prep_client(v[1],v[2],v[3]) then
-          logger.log(1,"{ find_fnet func } FORCED NETWORK: [ "..fn.." ] HAS BEEN CONFIGURED SUCCESSFULLY")
-          return true
-        else
-          logger.log(2,"{ find_fnet func } FORCED NETWORK: [ "..fn.." ] NOT AVAILABLE FOR CONFIGURATION")
-          return false
-        end
-      end
-    end
-    logger.log(2,"{ find_fnet func } FORCED NETWORK: [ "..fn.." ] NOT ACCESSIBLE")
-  end
- return false
-end
-M.find_fnet = find_fnet
-
 local check = function(ssid)
   local fn = get_fnet()
   if fn and fn == ssid then
-    if (logger.log_lev > 1) then logger.log(7,"FORCE NETWORK IS SET [ "..fn.." ] AND CONFIGURED") end
+    if (logger.log_lev > 1) then logger.log(7,"FORCED NETWORK IS SET [ "..fn.." ] AND CONFIGURED") end
     return false
   elseif fn then
-    if find_fnet(fn) then
+    logger.log(7,"ATTEMPTING TO CONFIGURE FORCED NETWORK [ "..fn.." ]")
+    if net.prep_client(fn) then
+      logger.log(7,"FORCE NETWORK IS SET [ "..fn.." ] AND CONFIGURED")
       return true
+    else
+     logger.log(7,"FAILED TO CONFIGURE FORCED NETWORK [ "..fn.." ]")
     end
   else
     if (logger.log_lev > 2) then logger.log(7,"{ fnet_check function } FORCE NETWORK NOT SET") end
